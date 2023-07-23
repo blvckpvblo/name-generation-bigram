ac7805d0da731577bc8c17f7b0ad84d26683d8b2
Momar Cisse <momar@thepinkchannel.com>
2023-07-23 20:06:11 +0000

    Init Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Name generation using Bigrams
+
+Inspired by Andrej Karpathy on Youtube. [YouTube Link](https://www.youtube.com/watch?v=PaCmpygFfXo)
---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+__author__ =  "Momar T. Cisse"
+
+def main():
+    """Main entry"""
+    print('Hello')
+
+
+if __name__ == "__main__":
+    """This is executed when run from the command line"""
+    main()
